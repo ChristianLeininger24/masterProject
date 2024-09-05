1424da95a4438243d0106f65ab54e3cc88c99b95
programmer <info2016frei@gmail.com>
2024-09-05 14:40:17 +0200

    add func create f3rm model

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,40 @@
 import yaml
 import torch
+import numpy as np
 from pathlib import Path
 from nerfstudio.models.nerfacto import NerfactoModelConfig, NerfactoModel
 from nerfstudio.data.scene_box import SceneBox
+
+from f3rm.model import FeatureFieldModelConfig, FeatureFieldModel
+
+
+
+
+def rotate_object(x_angle, y_angle):
+    """
+    Rotate an object in 3D space around the x-axis and y-axis.
+    
+    Parameters:
+    x_angle (float): The angle of rotation around the x-axis (in radians).
+    y_angle (float): The angle of rotation around the y-axis (in radians).
+    
+    Returns:
+    np.ndarray: The combined rotation matrix.
+    """
+    # Rotation matrix around the x-axis
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(x_angle), -np.sin(x_angle)],
+                    [0, np.sin(x_angle), np.cos(x_angle)]])
+    
+    # Rotation matrix around the y-axis
+    R_y = np.array([[np.cos(y_angle), 0, np.sin(y_angle)],
+                    [0, 1, 0],
+                    [-np.sin(y_angle), 0, np.cos(y_angle)]])
+    
+    # Combined rotation matrix
+    R = np.dot(R_y, R_x)
+
+    return R
 
 
 def load_state_dict(model, state_dict):
@@ -51,6 +83,27 @@ def get_model(model_path, config_path, device="cuda"):
     model = NerfactoModel(config=model_config, scene_box= scene_box, num_train_data=794).to(device)
     
     loaded_state = torch.load(model_path, map_location="cpu")
+    model.update_to_step(loaded_state["step"])
+    model = load_state_dict(model, loaded_state["pipeline"])
+    return model
+
+
+def get_f3rm_model(config_path: str, load_path: str, device):
+    """ This loads a pre trained model from the path specified in the config file"""
+    #config_path = Path("/home/programmer/master_project_experiments/07_12_2023/outputs/nerf_output/f3rm/2023-12-07_072525/config.yml")
+    config_path = Path(config_path)
+    config = yaml.load(config_path.read_text(), Loader=yaml.Loader)
+    model_config=FeatureFieldModelConfig(eval_num_rays_per_chunk=1 << 14)
+    aabb_scale = config.pipeline.datamanager.dataparser.scene_scale
+    scene_box = SceneBox(
+            aabb=torch.tensor(
+                [[-aabb_scale, -aabb_scale, -aabb_scale], [aabb_scale, aabb_scale, aabb_scale]], dtype=torch.float32
+            )
+        )
+    #TODO num data train needs to be from the file
+    model = FeatureFieldModel(model_config, scene_box=scene_box, num_train_data=838, metadata={"feature_dim": 768, "feature_type":"CLIP"}, device=device).to(device)
+    # load_path  = "/home/programmer/master_project_experiments/07_12_2023/outputs/nerf_output/f3rm/2023-12-07_072525/nerfstudio_models/step-000029999.ckpt"
+    loaded_state = torch.load(load_path, map_location="cpu")
     model.update_to_step(loaded_state["step"])
     model = load_state_dict(model, loaded_state["pipeline"])
     return model
